d8ad65e60b8ff61d91a0bf7a205051d1d2f7fc97
Das Fräulein*innen*außen <ronny.koehler3@googlemail.com>
2026-08-21 21:35:44 +0200

    Pfad der öffentlichen Client-API korrigieren

--- public/api/clients.php
+++ public/api/clients.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once __DIR__ . '/../../app/classes/Clients.php';
-require_once __DIR__ . '/../../app/classes/DeviceRegistry.php';
+require_once __DIR__ . '/../app/classes/Clients.php';
+require_once __DIR__ . '/../app/classes/DeviceRegistry.php';
 
 header('Content-Type: application/json; charset=utf-8');
 header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
